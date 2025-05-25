b6a0725630fa5c483b9139d50b8cc5a0f691e6b3
wutangyuan <wutangyuan1@gmail.com>
2025-05-25 16:51:13 +0800

    增加序列化和反序列化的验证Demo

---
+++ testJsonSerial/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json.Serialization;
+
+
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+TestJson json = new TestJson()
+{
+    Sn = "12233",
+    Id = "123"
+};
+
+
+var newtontest = Newtonsoft.Json.JsonConvert.SerializeObject(json);
+
+var itextJsonTest = System.Text.Json.JsonSerializer.Serialize(json);
+Console.ReadKey();
+
+
+
+public class TestJson
+{
+    [System.Text.Json.Serialization.JsonPropertyName("gwsn")]
+    public string Sn { get; set; }
+
+    [Newtonsoft.Json.JsonProperty("id")]
+    public string Id { get;set; }
+}
