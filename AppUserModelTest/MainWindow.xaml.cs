30540ca219d9a091477dde2ae55ff2d4b3b36dd5
wutangyuan <wutangyuan1@gmail.com>
2025-08-07 21:58:58 +0800

    新增demo

---
+++ AppUserModelTest/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppUserModelTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SourceInitialized += MainWindow_SourceInitialized;
+        }
+
+        private void MainWindow_SourceInitialized(object? sender, EventArgs e)
+        {
+            //SetPinning();
+        }
+
+        private void SetPinning()
+        {
+            //return;
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+
+            // Define the property key for System.AppUserModel.PreventPinning
+            NativeWin32.PROPERTYKEY propKey = new NativeWin32.PROPERTYKEY(new Guid("{9F4C2855-9F79-4B39-A8D0-E1D42DE1D5F3}"), 9);
+
+            // Get the IPropertyStore for the window
+            NativeWin32.IPropertyStore propStore;
+            int hr = NativeWin32.SHGetPropertyStoreForWindow(hwnd, ref NativeWin32.IID_IPropertyStore, out propStore);
+            if (hr != 0) // if failed
+            {
+                Marshal.ThrowExceptionForHR(hr);
+            }
+
+            try
+            {
+                // Create a PROPVARIANT with bool value: true
+                NativeWin32.PROPVARIANT pv = new NativeWin32.PROPVARIANT();
+                pv.SetValue(true);
+
+                // Set the property
+                propStore.SetValue(ref propKey, ref pv);
+
+                // We must free the PROPVARIANT
+                NativeWin32.PropVariantClear(ref pv);
+            }
+            finally
+            {
+                // Release the IPropertyStore
+                Marshal.ReleaseComObject(propStore);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetPinning();
+        }
+    }
+
+    public class NativeWin32
+    {
+        [DllImport("shell32.dll")]
+        public static extern int SHGetPropertyStoreForWindow(IntPtr hwnd, ref Guid riid, out IPropertyStore propertyStore);
+
+        [DllImport("ole32.dll")]
+        public static extern int PropVariantClear(ref PROPVARIANT pvar);
+
+        // Define IPropertyStore interface
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("886D8EEB-8CF2-4446-8D02-CDBA1DBDCF99")]
+        public interface IPropertyStore
+        {
+            void GetCount(out uint cProps);
+            void GetAt(uint iProp, out PROPERTYKEY pkey);
+            void GetValue(ref PROPERTYKEY key, out PROPVARIANT pv);
+            void SetValue(ref PROPERTYKEY key, ref PROPVARIANT pv);
+            void Commit();
+        }
+
+        // Define IID for IPropertyStore
+        public static Guid IID_IPropertyStore = new Guid("886D8EEB-8CF2-4446-8D02-CDBA1DBDCF99");
+
+        // Define PROPERTYKEY struct
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        public struct PROPERTYKEY
+        {
+            public Guid fmtid;
+            public uint pid;
+
+            public PROPERTYKEY(Guid fmtid, uint pid)
+            {
+                this.fmtid = fmtid;
+                this.pid = pid;
+            }
+        }
+
+        // Define PROPVARIANT structure (simplified, we'll use a simple one for bool)
+        [StructLayout(LayoutKind.Explicit)]
+        public struct PROPVARIANT
+        {
+            // We'll only implement the necessary part for boolean
+            [FieldOffset(0)] public ushort vt;
+            [FieldOffset(8)] public byte boolVal;
+
+            public void SetValue(bool value)
+            {
+                // VT_BOOL
+                vt = 11;
+                boolVal = value ? (byte)1 : (byte)0;
+            }
+        }
+
+    }
+}
---
+++ TestComRefenceNet8/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using NetFwTypeLib;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestComRefenceNet8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 添加防火墙例外端口
+        /// </summary>
+        /// <param name="name">名称</param>
+        /// <param name="port">端口</param>
+        /// <param name="protocol">协议(TCP、UDP)</param>
+        public static void NetFwAddPorts(string name, int port, string protocol)
+        {
+            //创建firewall管理类的实例
+            INetFwMgr netFwMgr = (INetFwMgr)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwMgr"));
+
+            INetFwOpenPort objPort = (INetFwOpenPort)Activator.CreateInstance(
+                Type.GetTypeFromProgID("HNetCfg.FwOpenPort"));
+
+            objPort.Name = name;
+            objPort.Port = port;
+            if (protocol.ToUpper() == "TCP")
+            {
+                objPort.Protocol = NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+            }
+            else
+            {
+                objPort.Protocol = NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_UDP;
+            }
+            objPort.Scope = NET_FW_SCOPE_.NET_FW_SCOPE_ALL;
+            objPort.Enabled = true;
+
+            bool exist = false;
+            //加入到防火墙的管理策略
+            foreach (INetFwOpenPort mPort in netFwMgr.LocalPolicy.CurrentProfile.GloballyOpenPorts)
+            {
+                if (objPort == mPort)
+                {
+                    exist = true;
+                    break;
+                }
+            }
+            if (!exist) netFwMgr.LocalPolicy.CurrentProfile.GloballyOpenPorts.Add(objPort);
+        }
+    }
+}
