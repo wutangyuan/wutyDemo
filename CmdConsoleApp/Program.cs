ac3ddd24b73ae820c9034e50548f07ef4fcfc620
wutangyuan <wutangyuan1@gmail.com>
2025-08-18 22:25:16 +0800

    增加命令行参数字符限制的demo

---
+++ CmdConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+if (args.Length <= 0)
+{
+    Console.WriteLine($"没有输入参数");
+    return;
+}
+Console.WriteLine($"输入参数个数: {args.Length}");
+foreach (var arg in args)
+{
+    Console.WriteLine($"参数为: {arg}");
+    Console.WriteLine($"参数的长度为：{arg.Length}");
+}
+
+
---
+++ CmdStarter/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Diagnostics;
+using System.Text;
+
+var cmdConsoleAppPath  = @$"E:\Code\wutyDemo\CmdConsoleApp\bin\Debug\net8.0\CmdConsoleApp.exe";
+var pathLength = cmdConsoleAppPath.Length;
+
+
+
+StringBuilder sb = new StringBuilder();
+
+//32,767
+
+
+var cmdFileName = "cmd.exe";
+var processArgs1 = $"/k {cmdConsoleAppPath} ";
+
+var cmdParmamterCount = 8188 - cmdFileName.Length - processArgs1.Length;
+for (int i = 0; i < cmdParmamterCount; i++)
+{
+    sb.Append("1");
+}
+processArgs1 += sb.ToString();
+
+using Process process = new Process();
+
+
+// 2. 配置启动信息
+process.StartInfo = new ProcessStartInfo
+{
+    FileName = cmdFileName,                 // 调用命令提示符
+    Arguments = processArgs1,   // 执行命令参数
+};
+process.Start();
+
+
+
+var count = 32763 - pathLength;
+
+sb = new StringBuilder();
+for (int i = 0; i < count; i++)
+{
+    sb.Append("1");
+}
+
+Process.Start(cmdConsoleAppPath, sb.ToString());
