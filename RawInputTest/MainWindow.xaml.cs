d77804c702d6976eace9912bb08fdc3cad8dd495
wutangyuan <wutangyuan1@gmail.com>
2025-08-24 22:35:04 +0800

    增加定时任务和全局触摸的demo

---
+++ RawInputTest/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Linearstar.Windows.RawInput;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RawInputTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
+            
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var windowInteropHelper = new WindowInteropHelper(this);
+            var hwnd = windowInteropHelper.Handle;
+
+            // Get the devices that can be handled with Raw Input.
+            var devices = RawInputDevice.GetDevices();
+
+            // register the keyboard device and you can register device which you need like mouse
+            RawInputDevice.RegisterDevice(HidUsageAndPage.Keyboard,
+                RawInputDeviceFlags.ExInputSink | RawInputDeviceFlags.NoLegacy, hwnd);
+
+            RawInputDevice.RegisterDevice(HidUsageAndPage.TouchScreen,
+                RawInputDeviceFlags.ExInputSink | RawInputDeviceFlags.DevNotify, hwnd);
+            RawInputDevice.RegisterDevice(HidUsageAndPage.Pen,
+                RawInputDeviceFlags.ExInputSink | RawInputDeviceFlags.DevNotify, hwnd);
+
+            HwndSource source = HwndSource.FromHwnd(hwnd);
+            source.AddHook(Hook);
+        }
+
+
+        private string text;
+
+        private IntPtr Hook(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam, ref bool handled)
+        {
+            const int WM_INPUT = 0x00FF;
+
+            // You can read inputs by processing the WM_INPUT message.
+            if (msg == WM_INPUT)
+            {
+                // Create an RawInputData from the handle stored in lParam.
+                var data = RawInputData.FromHandle(lparam);
+
+                // You can identify the source device using Header.DeviceHandle or just Device.
+                var sourceDeviceHandle = data.Header.DeviceHandle;
+                var sourceDevice = data.Device;
+
+                // The data will be an instance of either RawInputMouseData, RawInputKeyboardData, or RawInputHidData.
+                // They contain the raw input data in their properties.
+                switch (data)
+                {
+                    case RawInputMouseData mouse:
+                        Debug.WriteLine($"Mouse {mouse.Mouse}");
+                        break;
+                    case RawInputKeyboardData keyboard:
+                        Debug.WriteLine(keyboard.Keyboard);
+                        break;
+                    case RawInputHidData hid:
+                        Debug.WriteLine($"Hid {hid.Hid}");
+
+
+
+                        text = @$"DevicePath: {hid.Device.DevicePath}
+VID:{hid.Device.VendorId:X2} PID:{hid.Device.ProductId:X2}
+RawData:{hid.Hid}";
+
+                        Console.WriteLine(text);
+
+                        if (hid is RawInputDigitizerData rawInputDigitizerData)
+                        {
+                            foreach (var rawInputDigitizerContact in rawInputDigitizerData.Contacts)
+                            {
+                                text += rawInputDigitizerContact.ToString() + "\r\n";
+                            }
+                        }
+                        Console.WriteLine(text);
+                        break;
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ TimeTask/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Quartz;
+using Quartz.Impl;
+
+Console.WriteLine("Hello, World!");
+
+IJobDetail job = JobBuilder.Create<TestJob>()
+    .WithIdentity("TestJob", "myGroup")
+    .Build();
+
+
+//ITrigger trigger = TriggerBuilder.Create().WithIdentity("TestJobTrigger", "myGroup")
+//    .WithSimpleSchedule(x =>
+//    {
+//        x.WithIntervalInSeconds(1).RepeatForever();
+//    })
+//    .Build();
+
+
+ITrigger trigger = TriggerBuilder.Create()
+    .WithIdentity("TestJobTrigger", "myGroup")
+    .WithCronSchedule("0 15 10 * * ?")
+    .Build();
+
+
+StdSchedulerFactory factory = new StdSchedulerFactory();
+//创建任务调度器
+IScheduler scheduler = await factory.GetScheduler();
+//启动任务调度器
+await scheduler.Start();
+
+//将创建的任务和触发器条件添加到创建的任务调度器当中
+await scheduler.ScheduleJob(job, trigger);
+
+Console.WriteLine("任务调度器已启动，按任意键退出...");
+Console.ReadKey();
+
+
+
+/// <summary>
+/// 创建一个测试的Job类
+/// </summary>
+public class TestJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        Console.WriteLine($"{DateTime.Now.ToString("yy-MM-dd HH:mm:ss fff")},执行了TestJob");
+        await Task.CompletedTask;
+    }
+}
